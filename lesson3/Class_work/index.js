4ba92ddc46b83ca909cbb9a67e2777f49163b571
John Doe <ealezhnina@yandex.ru>
2021-10-06 19:57:31 +0300

    Дз

---
+++ lesson3/Class_work/index.js
@@ -0,0 +1,63 @@
+// const firstValue = String(5);
+
+// console.log(typeof firstValue);
+
+// const obj = {
+// foo: 55,
+// };
+
+//console.log(typeof String(obj.foo));
+
+//let testValue;
+
+//console.log(typeof testValue);
+
+//testValue = Number(testValue);
+
+//console.log(testValue);
+
+//const isUser = true;
+
+//const isAdmin = false;
+
+//console.log(Number(isUser), Number(isAdmin));
+
+//const voldString = "";
+
+//console.log(Number(voldString));
+
+//BOOLEAN
+
+// const value = 30;
+// console.log(typeof Boolean(value), Boolean(value));
+// const obj = [" "].length;
+// console.log(Boolean(obj));
+
+// Унарный плюс
+
+// const value = "5";
+// console.log(typeof value, typeof +value, typeof Number(value));
+
+// Бинарный плюс
+
+// let sum = 5 + 6;
+// console.log(sum);
+
+// sum = 1 + true;
+// console.log(sum);
+
+// sum = 5 + null;
+// console.log(sum);
+
+// const stringValue = "12";
+// const numberValue = 25;
+
+// console.log(+stringValue + numberValue);
+// console.log(typeof (numberValue + +stringValue));
+
+const firstName = "Kate";
+const lastName = "Lezhnina";
+
+console.log(firstName + " " + lastName);
+
+console.log(`${firstName} ${lastName}`);
