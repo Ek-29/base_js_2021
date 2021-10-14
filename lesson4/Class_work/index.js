8eba752a281702d56218b46b38f4f651c91d835e
John Doe <ealezhnina@yandex.ru>
2021-10-14 21:18:52 +0300

    дз

---
+++ lesson4/Class_work/index.js
@@ -0,0 +1,94 @@
+console.log("**Start**");
+// const value = 5;
+// if (value === 0) {
+//   console.log(value, " = value");
+// } else if (value > 1) {
+//   console.log(value, " > 1");
+// } else if (value < 1) {
+//   console.log(value, " < 1");
+// } else {
+//   console.log(value);
+// }
+
+// const age = 20;
+// const result = age >= 18 ? "Доступ разрешен" : "Доступ запрещен";
+// console.log(result);
+
+// switch ("Kate") {
+//   case "Elena":
+//     console.log("Elena");
+//     break;
+//   case "Kate":
+//     console.log("Kate");
+//     break;
+//   case "Marina":
+//     console.log("Marina");
+//     break;
+//   default:
+//     console.log("Имени нет в списке");
+// }
+
+// let length_1 = 12;
+// let length_2 = 40;
+// let length_3 = 5;
+// if ((length_1 > length_2) & length_3) {
+//   console.log(length_1);
+// }
+// if ((length_2 > length_1) & length_3) {
+//   console.log(length_2);
+// } else {
+//   console.log(length_3);
+// }
+
+// let wthr = 19;
+// if (wthr < -30) {
+//   console.log("Оставайтесь дома");
+// }
+// if ((wthr < -10) & (wthr > -30)) {
+//   console.log("Сегодня холодно");
+// }
+// if ((wthr < 5) & (wthr > -10)) {
+//   console.log("Не холодно");
+// }
+// if ((wthr < 15) & (wthr > 5)) {
+//   console.log("Тепло");
+// }
+// if ((wthr < 25) & (wthr > 15)) {
+//   console.log("Очень тепло");
+// } else if ((wthr < 35) & (wthr > 25)) {
+//   console.log("Жарко");
+// } else {
+//   console.log("Пекло!");
+// }
+
+// switch ("Engineer") {
+//   case "Manager":
+//     console.log("Elena");
+//     break;
+//   case "Admin":
+//     console.log("Max");
+//     break;
+//   case "User":
+//     console.log("Alex");
+//     break;
+//   default:
+//     console.log("Роль не значится");
+// }
+
+// while (true) {
+//   console.log("loop");
+
+// console.log(end);
+
+// let i = 0;
+// while (i < 10) {
+//   console.log(i);
+//   i += 1;
+// }
+// console.log("end");
+let i = 0;
+do {
+  console.log("work");
+} while (i < 1);
+console.log("end");
+console.log("**Finish**");
